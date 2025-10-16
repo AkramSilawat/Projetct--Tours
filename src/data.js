0041dbc4cdbb0a8238ade3f7f5f02f0e98307200
Aman Silawat <Akram.khan.silawat.aks@gmail.com>
2025-10-16 16:28:42 +0530

    Project Tours

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        name: 'Bertie Yates',
+       price: `$5800`,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+        id: 2,
+        name: 'Hester Hogan',
+        price: `$3200`,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+        id: 3,
+        name: 'Larry Little',
+        price: `$8200`,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+        id: 4,
+        name: 'Sean Walsh',
+       price: `$7200`,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+        id: 5,
+        name: 'Lola Gardner',
+       price: `$4800`,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
